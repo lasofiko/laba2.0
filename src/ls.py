25d083f5db50bc04757bde416ead4dafec4e861a
lasofiko <la.sofaa@yandex.ru>
2025-11-14 23:44:35 +0300

    ls.py

--- src/ls.py
+++ src/ls.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from lg import lg
 
+
 def ls_c(p='.'):
     try:
         a = p.split()
@@ -14,28 +15,29 @@ def ls_c(p='.'):
             else:
                 tp = i
         it = os.listdir(tp)
-        
+        # возвращает список файлов и папок в указанной директории
+
         if t:
             print(f'Содержимое {tp}:')
             for i in it:
-                fp = os.path.join(tp, i)
-                st = os.stat(fp)
-                sz = st.st_size
-                dt = datetime.fromtimestamp(st.st_mtime)
-                date_s = dt.strftime('%d.%m.%Y %H:%M')
+                fp = os.path.join(tp, i)  # полный путь к файлу/папке
+                st = os.stat(fp)  # получение метаданных
+                sz = st.st_size  # размер в байтах
+                dt = datetime.fromtimestamp(st.st_mtime)  # время изменения
+                date_s = dt.strftime('%d.%m.%Y %H:%M')  # форматирование даты
                 if os.path.isdir(fp):
                     print(f'папка   {sz:8} {date_s} {i}')
                 else:
                     print(f'файл    {sz:8} {date_s} {i}')
         else:
             for i in it:
-                fp= os.path.join(tp, i)
+                fp = os.path.join(tp, i)
                 if os.path.isdir(fp):
                     print(i + '/')
                 else:
                     print(i)
         lg(f'ls {p}')
-        
+
     except FileNotFoundError:
         print(f"Ошибка: Папка '{p}' не найдена")
         lg(f'ls {p}', False)
