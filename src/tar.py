81227efd8477787bba756a4f89ab53c6125a69e7
lasofiko <la.sofaa@yandex.ru>
2025-11-14 23:15:39 +0300

    tar.py

--- src/tar.py
+++ src/tar.py
@@ -5,10 +5,10 @@ import tarfile
 def tar_c(a):
     try:
         with tarfile.open(a, "w:gz") as tar:
-            # 1- имя архива, 2- режим записи с gzip-сжатием
+            #  2- режим записи с gzip-сжатием
             tar.add('.', arcname=os.path.basename('.'))
             # добавляет в архив текущую директорию и все ее содержимопе 
-            # arcname=os.path.basename('.') - задает имя корневой папки в архиве
+            # 2 - задает имя корневой папки в архиве
         lg(f"tar {a}")
         print(f"Создан архив {a}")
     except Exception as e:
