2278767d1ad055a7c2c719522c1885c86a0c20be
lasofiko <la.sofaa@yandex.ru>
2025-11-14 23:41:33 +0300

    cd.py

--- src/cd.py
+++ src/cd.py
@@ -4,12 +4,12 @@ import shutil
 def cd_c(p):
     try:
         if p == '..':
-            os.chdir('..') #os.chdir('..') - переходит в родительскую папку
+            os.chdir('..') #переходит в родительскую папку
         elif p == '~':
-            os.chdir(os.path.expanduser('~')) #os.path.expanduser('~') - преобразует ~ в полный путь к домашней папке
+            os.chdir(os.path.expanduser('~')) #преобразует ~ в полный путь к домашней папке
         else:
             os.chdir(p) #Переход в указанную директорию:
-        print(f'Текущая папка: {os.getcwd()}') #os.getcwd() - возвращает текущую рабочую директорию
+        print(f'Текущая папка: {os.getcwd()}')
         lg(f'cd {p}')
     except Exception as e:
         print(f'Ошибка: {e}')
